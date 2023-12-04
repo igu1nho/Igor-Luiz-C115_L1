0650f63bf3a502356b8494148d21cf4e06f451ee
Igor Luiz Rodrigues <89806466+igu1nho@users.noreply.github.com>
2023-12-03 22:19:56 -0300

    Add files via upload

---
+++ Trabalho_IOT/ph_meter.ino
@@ -0,0 +1,57 @@
+#include <ESP8266WiFi.h>
+
+// Configurações do Wi-Fi e ThingSpeak
+const char* ssid = "Casarao_2.4GHz"; // Substitua com o nome da sua rede Wi-Fi
+const char* password = "chopp2021"; // Substitua com a senha da sua rede Wi-Fi
+const char* server = "api.thingspeak.com"; // Servidor do ThingSpeak
+String apiKey = "4XRQUUDKQZUNSEXX"; // Sua chave de API do ThingSpeak
+
+const int potPin = A0; // Pino do potenciômetro
+
+WiFiClient client;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(potPin, INPUT);
+
+  // Conexão Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(2000);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  int sensorValue = analogRead(A0);
+  float ph = sensorValue * (14.0/1023.0);
+
+  Serial.print("PH: ");
+  Serial.println(ph);
+
+  // Envia para o ThingSpeak
+  if (client.connect(server, 80)) {
+    String postStr = apiKey;
+    postStr += "&field1=";
+    postStr += String(ph);
+    postStr += "\r\n";
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+  }
+
+  delay(1000); // Aguarda antes de enviar o próximo valor
+}
